0982094b1bfc8d9b9d68fb299f8807fac0a89646
Jonathan Harwood <jhharwood88@gmail.com>
2018-11-26 01:13:38 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -32,7 +32,7 @@ def using_concat
 end
 
 def using_insert
-  
+  list_of_esoteric_programming_languages = ["Ante", "ArnoldC", "Befunge", "Binary lambda calculus", "Chef", "GolfScript", "Ook!"]
 end
 
 def using_uniq
