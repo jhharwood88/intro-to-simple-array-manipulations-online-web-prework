4ffa2cca1698244c6f2b32e260e874b7ef052f6d
Jonathan Harwood <jhharwood88@gmail.com>
2018-11-26 01:29:49 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,12 +1,10 @@
 def using_push(countries_in_western_africa, updated_array)
-  #countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
-  new_country = "Niger"
+  #new_country = "Niger"
   updated_array = countries_in_western_africa.push(new_country)
 end
 
 def using_unshift(neighborhoods_in_northwest_brooklyn, updated_array)
-  #neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
-  new_neighborhood = "Brooklyn Heights"
+  #new_neighborhood = "Brooklyn Heights"
   updated_array = neighborhoods_in_northwest_brooklyn.unshift(new_neighborhood)
 end
 
