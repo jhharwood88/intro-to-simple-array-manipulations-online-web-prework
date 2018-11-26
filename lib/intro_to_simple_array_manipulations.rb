e80570d6135dca21e6889afc1436c294cb45aee6
Jonathan Harwood <jhharwood88@gmail.com>
2018-11-26 01:42:30 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -40,8 +40,8 @@ def using_flatten(private_colleges_in_newyork)
   private_colleges_in_newyork.flatten
 end
 
-def using_delete(instructors, no)
-  instructors.delete("Steven")
+def using_delete(instructors, no_offense_steven)
+  instructors.delete(no_offense_steven)
 end
 
 def using_delete_at(famous_robots)
