5c5ca5689d067466a43764ee87e8adca3224ab4f
Jonathan Harwood <jhharwood88@gmail.com>
2018-11-26 01:31:50 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -17,35 +17,31 @@ def pop_with_args(chars_in_game_of_thrones)
 end
 
 def using_shift(my_favorite_cities)
+  
+end
+
+def shift_with_args(ice_cream_brands)
+
+def using_concat(my_favorite_things, more_favs)
 
 end
 
-def shift_with_args
-  ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
+def using_insert (list_of_esoteric_programming_languages, another_esoteric_language)
+  
 end
 
-def using_concat
-  my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
-  more_favs = ["mario kart", "flatiron school"]
+def using_uniq(captain_planet_and_the_planeteers)
+  
 end
 
-def using_insert
-  list_of_esoteric_programming_languages = ["Ante", "ArnoldC", "Befunge", "Binary lambda calculus", "Chef", "GolfScript", "Ook!"]
-  another_esoteric_language = "Malbolge"
+def using_flatten(private_colleges_in_newyork)
+  
 end
 
-def using_uniq
-  captain_planet_and_the_planeteers = ["Captain Planet", "Gaia", "Kwame", "Gi", "Linka", "Wheeler", "Gaia"]
+def using_delete(instructors)
+  
 end
 
-def using_flatten
-  private_colleges_in_newyork = ["New York University", ["Manhattan School of Music ", "Columbia University"], "The Juilliard School", "Bard College", "Cooper Union"]
-end
-
-def using_delete
-  instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
-end
-
-def using_delete_at
-  famous_robots = ["the dog from doctor who", "R2D2", "Ultron"]
+def using_delete_at(famous_robots)
+  
 end
